01b18a752c543436239c90ab0f05b65430dbd042
Kevinstronger <zhang1830@gmail.com>
2019-06-07 16:54:09 +0800

    增加带参数的Get请求测试用例

--- src/main/java/com/tsing/getMethod.java
+++ src/main/java/com/tsing/getMethod.java
@@ -1,20 +1,26 @@
 package com.tsing;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.Locale;
-import java.util.ResourceBundle;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
 
 public class getMethod {
 
@@ -45,5 +51,29 @@ public class getMethod {
         Assert.assertEquals(result,"收到cookie，访问成功！");
     }
 
+    @Test
+    public void getParamTest() throws Exception {
+        client = HttpClients.createDefault();
+//        第一种添加参数的方式
+//        URI uri = new URIBuilder("http://localhost:8890/get/with/param")
+//                .setParameter("startPage", "1")
+//                .setParameter("endPage","10").build();
+
+//        第二种天剑参数的方式
+        URIBuilder uri = new URIBuilder("http://localhost:8890/get/with/param");
+        List<NameValuePair> params = new LinkedList<>();
+        BasicNameValuePair startPage = new BasicNameValuePair("startPage", "10");
+        BasicNameValuePair endPage = new BasicNameValuePair("endPage", "20");
+        params.add(startPage);
+        params.add(endPage);
+        uri.setParameters(params);
+        HttpGet get = new HttpGet(uri.build());
+        CloseableHttpResponse response = client.execute(get);
+        String content = EntityUtils.toString(response.getEntity(),"UTF-8");
+        JSONObject goods = new JSONObject(content);
+        System.out.println(goods.get("蓝牙耳机"));
+        Assert.assertEquals(goods.get("蓝牙耳机"),256.9);
+
+    }
 
 }
